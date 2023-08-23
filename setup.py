1f931426323f67b00b46289c6b8b533ed3d9a571
Steve Munson <steve.munson@gmail.com>
2023-08-23 10:04:51 -0400

    Is this a library?

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pyapi',
+    packages=['pyapi'],
+    version='0.1.0',
+    description='A library for communication with APIs',
+    author='Steve Munson',
+    author_email='steve.munson@gmail.com',
+    license='GNU3',
+    install_requires=['requests'],
+)
